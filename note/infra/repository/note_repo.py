c12bacb2e52c90f4b7d84effe1485fc46a40f54a
smu202110806 <100983190+smu202110806@users.noreply.github.com>
2025-01-21 18:39:00 +0900

    ch.09 comp

--- note/infra/repository/note_repo.py
+++ note/infra/repository/note_repo.py
@@ -46,17 +46,109 @@ class NoteRepository(INoteRepository):
         return NoteVO(**row_to_dict(note))
         # raise NotImplementedError
     
-    def save(self, user_id: str, note: Note) -> Note:
-        raise NotImplementedError
+    def save(self, user_id: str, note_vo: NoteVO):
+        with SessionLocal() as db:
+            tags: list[Tag] = []
+            for tag in note_vo.tags:
+                existing_tag = db.query(Tag).filter(Tag.name == tag.name).first()
+                if existing_tag:
+                    tags.append(existing_tag)
+                else:
+                    tags.append(
+                        Tag(
+                            id=tag.id,
+                            name=tag.name,
+                            created_at=tag.created_at,
+                            updated_at=tag.updated_at,
+                        )
+                    )
+        new_note = Note(
+            id=note.vo.id,
+            user_id=user_id,
+            title=note_vo.title,
+            content=note_vo.content,
+            memo_date=note_vo.memo_date,
+            tags=tags,
+            created_at=note_vo.created_at,
+            updated_at=note_vo.updated_at,
+        )
+        
+        db.add(new_note)
+        db.commit
+        # raise NotImplementedError
     
-    def update(self, user_id: str, note: Note) -> Note:
-        raise NotImplementedError
+    def update(self, user_id: str, note_vo: Note_VO) -> NoteVO:
+        with SessionLocal() as db:
+            self.delete_tags(user_id, note_vo.id)
+            
+            note = (
+                db.query(Note)
+                .filter(Note.user_id == user_id, Note.id == note_vo.id)
+                .first()
+            )
+            if not note:
+                raise HTTPException(status_code=422)
+            
+            note.title = note_vo.title
+            note.content = note_vo.content
+            note.memo_date = note_vo.memo_date
+            
+            tags: list[Tag] = []
+            for tag in note_vo.tags:
+                existing_tag = db.query(Tag).filter(Tag.name == tag.name).first()
+                
+            if existing_tag:
+                tag.append(existing_tag)
+            else:
+                tags.append(
+                    Tag(
+                        id=tag.id,
+                        name=tag.name,
+                        created_at=tag.created_at,
+                        updated_at=tag.updated_at,
+                    )
+                )
+                
+        note.tags = tags
+        
+        db.add(note)
+        db.commit()
+        
+        return NoteVO(**row_to_dict(note))
+        # raise NotImplementedError
     
     def delete(self, user_id: str, id: str):
-        raise NotImplementedError
+        with SessionLocal() as db:
+            self.delete_tags(user_id, id)
+            
+            note = db.query(Note).filter(
+                Note.user_id == user_id, Note.id == id
+            ).first()
+            if not note:
+                raise HTTPException(status_code=422)
+            
+            db.delete(note)    
+            db.commit()
+        # raise NotImplementedError
     
-    def delete_tag(self, user_id: str, id: str):
-        raise NotImplementedError
+    def delete_tags(self, user_id: str, id: str):
+        with SessionLocal() as db:
+            note = db.query(Note).filter(
+                Note.user_id == user_id, Note.id == id
+            ).first()
+            if not note:
+                raise HTTPException(status_code=422)
+            
+            note.tags = []
+            db.add(note)
+            db.commit()
+            
+            unused_tags = db.query(Tag).filter(~Tag.notes.any()).all()
+            for tag in unused_tags:
+                db.delete(tag)
+                
+            db.commit()
+        # raise NotImplementedError
     
     def get_notes_by_tag_name(
         self,
@@ -64,5 +156,30 @@ class NoteRepository(INoteRepository):
         tag_name: str,
         page: int,
         items_per_page: int,
-    ) -> tuple[int, list[Note]]:
+    ) -> tuple[int, list[NoteVO]]:
+        with SessionLocal() as db:
+            tag = db.query(Tag).filter_by(name=tag_name).first()
+            
+            if not tag:
+                return 0, []
+            
+            query = (
+                db.query(Note)
+                # .options(joinedload(Note.tags))
+                .filter(
+                    Note.user_id == user_id,
+                    Note.tags.any(id=tag.id),
+                )
+            )
+            
+            total_count = query.count()
+            notes = (
+                query.offset((page - 1) * items_per_page).limit(items_per_page).all()
+            )
+            
+            note_vos = [NoteVO(**row_to_dict(note)) for note in notes]
+            
+            return total_count, note_vos
+        
+        
         raise NotImplementedError
